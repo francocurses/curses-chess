2dcab816cfea2fa1cec43fcdc7e0b4994595e71e
Franco Curotto <francocurotto@gmail.com>
2020-08-31 18:20:10 -0400

    update

--- source/display.py
+++ source/display.py
@@ -6,21 +6,24 @@ class Display():
     The guy in charge of displaying the board.
     """
     def __init__(self,stdscr):
-        # used colors
+        # used colors and attributes
         init_pair(1,COLOR_RED,COLOR_BLACK)
-        red = color_pair(1)
-
+        self.red = color_pair(1)
+        init_pair(2,COLOR_BLACK,COLOR_YELLOW)
+        self.bkgy = color_pair(2)
+        init_pair(3,COLOR_BLACK,COLOR_GREEN)
+        self.bkgg = color_pair(3)
 
         # display window
         self.dw = newwin(12,12,0,0)
         self.dw.box()
 
         # draw coordinates in window
-        self.dw.addstr(1, 2,"abcdefgh",red)
-        self.dw.addstr(10,2,"abcdefgh",red)
+        self.dw.addstr(1, 2,"abcdefgh",self.red)
+        self.dw.addstr(10,2,"abcdefgh",self.red)
         for i in range(1,9):
-            self.dw.addstr(10-i, 1,str(i),red)
-            self.dw.addstr(10-i,10,str(i),red)
+            self.dw.addstr(10-i, 1,str(i),self.red)
+            self.dw.addstr(10-i,10,str(i),self.red)
 
         # board window
         self.bw = self.dw.derwin(8,8,2,2)
@@ -38,20 +41,30 @@ class Display():
         for y in range(cols):
             for x in range(rows):
                 piece = m[y,x]
-                if piece is None:
-                    addstr(self.bw,y,x,"X")
-                else:
-                    c = piece.char
-                    addstr(self.bw,y,x,c)
+                self.drawpiece(piece,y,x)
         self.bw.refresh()
 
-def addstr(w,y,x,s):
+    def drawpiece(self,piece,y,x):
+        """
+        Draw a chess piece in the board. If piece
+        is None, then draw nothing.
+        """
+        # choose color depending in position
+        color = self.bkgy if (x+y)%2 else self.bkgg
+    
+        # draw piece character
+        char = " "
+        if piece is not None:
+            char = piece.char
+        addstr(self.bw,y,x,char,color)
+
+def addstr(w,y,x,s,a=A_NORMAL):
     """
     Add string s into window w at position (y,x)
     safetely, meaning you won't deal with the
     curses error in the bottom right corner
     """
     try:
-        w.addstr(y,x,s)
+        w.addstr(y,x,s,a)
     except _curses.error as e:
         pass
